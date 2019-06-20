01e93bc69a382017d94599296385a3384b9b82b7
Daniel Micay <danielmicay@gmail.com>
2019-06-20 12:48:08 -0400

    b4s4: Disable system_other odex
    Change-Id: Iea7207578639d4ba8de6912718b3d6e21d13090f

--- device.mk
+++ device.mk
@@ -122,7 +122,6 @@ MASTER_SIDE_CP_TARGET_LIST := sdm710 # ION specific settings
 # A/B support
 PRODUCT_PACKAGES += \
     otapreopt_script \
-    cppreopts.sh \
     update_engine \
     update_verifier
 
@@ -132,9 +131,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_PACKAGES += \
     bootctrl.sdm710
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cp_system_other_odex=1
 
 # Script that copies preloads directory from system_other to data partition
 PRODUCT_COPY_FILES += \
