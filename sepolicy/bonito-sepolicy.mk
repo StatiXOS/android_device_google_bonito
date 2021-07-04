5928c56a9c5312cb9f27fee5e0468049d991d278
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-07-04 15:44:31 -0400

    bonito: Include google_battery and confirmationhal policy
    * These services should in theory have access to whatever node they
      polling to however, digging deeper we can see that these are not
      labeled anywhere in device specific sepolicy.
    
    * Grab the labels and contexts from hardware/google/pixel-sepolicy
    
    * Stuff like the battery service is now very happy about the permissions
      it didn't have before.

--- sepolicy/bonito-sepolicy.mk
+++ sepolicy/bonito-sepolicy.mk
@@ -7,3 +7,8 @@ BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/qcom/sdm710
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/google
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/verizon
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/tracking_denials
+
+# Pixel-wide sepolicy
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/confirmationui_hal
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/turbo_adapter
